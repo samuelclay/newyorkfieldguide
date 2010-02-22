8893e9c020f920e970ecd6ee0bc7db81b395853c
samuelclay <samuel@ofbrooklyn.com>
2010-02-21 22:02:58 -0500

    Template tags for post detail

---
+++ posts/templatetags/post_tags.py
@@ -0,0 +1,9 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+def highlight(text, word):
+    return mark_safe(text.replace(word, "<span class='highlight'>%s</span>" % word))
+    
