5ad073601c428f9d5814d22508081922e03c2ab8
Samuel Clay <samuel@ofbrooklyn.com>
2016-01-08 12:56:31 -0800

    Switching flickr from _b photos to _z photos.

--- syncr/flickr/models.py
+++ syncr/flickr/models.py
@@ -118,7 +118,7 @@ class Photo(models.Model):
 
     def get_large_url(self):
         if self.has_large_photo:
-            return self._get_photo_url_helper('b')
+            return self._get_photo_url_helper('z')
         return self.get_original_url()
 
     def get_original_url(self):
